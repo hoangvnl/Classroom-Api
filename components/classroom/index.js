57e3fdc37f654efaa064f0abc772dc8df1c6ed88
Hoang <hoangvnl57@gmail.com>
2021-10-29 19:48:58 +0700

    add connectionString

--- components/classroom/index.js
+++ components/classroom/index.js
@@ -8,6 +8,8 @@ const pool = new Pool({
   database: "d4alf9m85cjslp",
   password: "dfc83b5ce545ac073d1402acce404c635e709d9fef4a6cb2d43e7fb3b2f317a3",
   port: 5432,
+  connectionString:
+    "postgres://ibrsfpixxqekcm:dfc83b5ce545ac073d1402acce404c635e709d9fef4a6cb2d43e7fb3b2f317a3@ec2-35-169-204-98.compute-1.amazonaws.com:5432/d4alf9m85cjslp",
   ssl: {
     rejectUnauthorized: false,
   },
